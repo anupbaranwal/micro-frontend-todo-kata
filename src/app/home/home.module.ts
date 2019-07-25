0a6d63d03b5915927a9c7da9421e43e122b362fd
Anup Kumar <anup.raj1989@gmail.com>
2019-07-25 22:30:20 +0530

    feat(create-todo): Added new micro frontend component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import {CreateTodoModule} from '../../../projects/create-todo/src/app/create-todo.module'
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CreateTodoModule
   ]
 })
 export class HomeModule { }
